c494469e8c1067dad6aa9d8e7bd1ac14f078ae66
cgung7 <cgung7@naver.com>
2025-12-09 15:24:11 +0900

    20251209 demo_board

--- demo_ssr_v1_1/src/main/java/org/example/demo_ssr_v1_1/board/BoardController.java
+++ demo_ssr_v1_1/src/main/java/org/example/demo_ssr_v1_1/board/BoardController.java
@@ -1,4 +1,50 @@
 package org.example.demo_ssr_v1_1.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller // IoC
 public class BoardController {
+
+    @Autowired
+    private BoardPersistRepository boardPersistRepository;
+
+    // 게시글 화면 요청 - 자원요청 get
+    // http://localhost:8080/board/save-form
+    @GetMapping("/board/save-form")
+    public String saveForm() {
+
+        return "board/save-form";
+    }
+    // 게시글 작성 기능
+    // http://localhost:8080/board/save-form
+    @PostMapping("/board/save-form")
+    public String saveFormProc(@RequestParam("username") String username,
+                               @RequestParam("title") String title,
+                               @RequestParam("content") String content
+    ) {
+        System.out.println("username: " + username);
+        System.out.println("title: " + title);
+        System.out.println("content: " + content);
+
+        Board board = new Board();
+        board.setUsername(username);
+        board.setTitle(title);
+        board.setContent(content);
+
+        boardPersistRepository.save(board);
+
+        return "redirect:/";
+    }
+
+    // 게시글 목록 보기
+    // http://localhost:8080/board/list
+    @GetMapping("/board/list")
+    public String list() {
+
+        return "board/list";
+    }
 }
