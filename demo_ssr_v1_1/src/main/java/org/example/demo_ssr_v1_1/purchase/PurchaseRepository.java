9126972353ef24e12f14070714a7efe1d85fafb3
cgung7 <cgung7@naver.com>
2026-01-06 10:44:31 +0900

    20260106 게시판 유료 결제 목록 기능

--- demo_ssr_v1_1/src/main/java/org/example/demo_ssr_v1_1/purchase/PurchaseRepository.java
+++ demo_ssr_v1_1/src/main/java/org/example/demo_ssr_v1_1/purchase/PurchaseRepository.java
@@ -1,10 +1,8 @@
 package org.example.demo_ssr_v1_1.purchase;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,18 +12,19 @@ public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
     // Purchase p, Board b b.title, b.user.username
     //
     @Query("""
-        SELECT p FROM Purchase p 
-        JOIN FETCH p.board b
-        JOIN FETCH b.user
-        WHERE p.user.id = :userId
-        ORDER BY p.createdAt DESC
-""")
-    List<Purchase> findAllByUserIdWithBoard(@Param("userId")Long userId);
+                    SELECT p FROM Purchase p 
+                    JOIN FETCH p.board b
+                    JOIN FETCH b.user
+                    WHERE p.user.id = :userId
+                    ORDER BY p.createdAt DESC
+            """)
+    List<Purchase> findAllByUserIdWithBoard(@Param("userId") Long userId);
 
     // 사용자와 게시글의 구매 내역 조회
     @Query("SELECT p FROM Purchase p WHERE p.user.id = :userId AND p.board.id = :boardId")
     Optional<Purchase> findByUserIdAndBoardId(@Param("userId") Long userId,
                                               @Param("boardId") Long boardId);
+
     // 사용자와 게시글의 구매 여부 확인
     // default <- 자바 8+ 지원해주는 키워드
     default boolean existsByUserIdAndBoardId(Long userId, Long boardId) {
